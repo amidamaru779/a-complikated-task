eafbb653611d0cd14e08dda5bf1b863afa1eaefd
Nikita Polev <nikitko779@gmail.com>
2024-08-02 22:20:12 +0300

    вывел первые две цифры числа

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const num = 266219
 // создал переменную со значением number
-const digits = Array.from(String(num), Number);
+const digits = Array.from(String(num), Number)
 // преобразовал тип number в тип string
 console.log(digits);
 // вывел массив 
@@ -10,9 +10,13 @@ const mult = digits[0]*digits[1]*digits[2]*digits[3]*digits[4]*digits[5];
 console.log(mult);
 // вывел в консоль произведение цифр 
 
-const pow = mult**3
+const pow = mult**3;
 /* в новой переменной произвел возведение в 3 степень 
 результат переменной mult через оператор "**" */
 console.log(pow);
 // вывел в консоль произведение цифр в 3 степени 
 
+const firstdigit = Array.from(String(pow));
+// создал новую переменную в которой преобразовал результат pow в массив
+console.log(firstdigit[0],firstdigit[1]);
+// вывел первые два элемента массива поиндексно
