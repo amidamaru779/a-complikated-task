8adee5728c5523e3289a3a15f84c532c1bc53d52
Nikita Polev <nikitko779@gmail.com>
2024-08-04 23:30:02 +0300

    возвел результат произведения и вывел последние 2 цифры

--- script.js
+++ script.js
@@ -7,3 +7,10 @@ while (num > 0) {
     multiplication *= num % 10;
     num = Math.floor(num / 10);
 } console.log(multiplication);
+
+// Возводим результат в степень 3
+ let result = multiplication ** 3 
+
+// Первые две цифры числа
+let firstTwoDigits = result.toString()
+console.log(firstTwoDigits.slice(0,2));
