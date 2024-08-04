46601cd4812a8d52602c37f2f9eca9c9fd290968
Nikita Polev <nikitko779@gmail.com>
2024-08-04 23:15:39 +0300

    создал переменную и перемножил цифры числа

--- script.js
+++ script.js
@@ -1,22 +1,9 @@
-const num = 266219
-// создал переменную со значением number
-const digits = Array.from(String(num), Number)
-// преобразовал тип number в тип string
-console.log(digits);
-// вывел массив 
-const mult = digits[0]*digits[1]*digits[2]*digits[3]*digits[4]*digits[5];
-/* поиндексно перемножил между собой каждый элемент массива 
-и занес в новую переменную */
-console.log(mult);
-// вывел в консоль произведение цифр 
+// Создаем переменную num со значением 266219
+let num = 266219;
 
-const pow = mult**3;
-/* в новой переменной произвел возведение в 3 степень 
-результат переменной mult через оператор "**" */
-console.log(pow);
-// вывел в консоль произведение цифр в 3 степени 
-
-const firstdigit = Array.from(String(pow));
-// создал новую переменную в которой преобразовал результат pow в массив
-console.log(firstdigit[0],firstdigit[1]);
-// вывел первые два элемента массива поиндексно
+// Умножаем цифры числа
+let multiplication = 1;
+while (num > 0) {
+    multiplication *= num % 10;
+    num = Math.floor(num / 10);
+} console.log(multiplication);
