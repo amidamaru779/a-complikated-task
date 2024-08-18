15f1a0066050aa144f315cbdc8ac15ca9f430d68
Nikita Polev <nikitko779@gmail.com>
2024-08-18 16:59:15 +0300

    выполнил усложненное задание урока 7

--- script.js
+++ script.js
@@ -1,74 +1,23 @@
-<<<<<<< HEAD
-// Создаем переменную num со значением 266219
-let num = 266219;
-
-// Умножаем цифры числа
-let multiplication = 1;
-while (num > 0) {
-    multiplication *= num % 10;
-    num = Math.floor(num / 10);
-} console.log(multiplication);
-
-// Возводим результат в степень 3
- let result = multiplication ** 3 
-
-// Первые две цифры числа
-let firstTwoDigits = result.toString()
-console.log(firstTwoDigits.slice(0,2));
-=======
-'use strict';
-
-// через if 
-let lang = prompt('На каком языке вы хотите увидеть дни недели?', 'ru,en');
-let russianDaysOfTheWeek = "Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье";
-let englishDaysOfTheWeek = "Monday, Thuesday, Wednesday, Thurday, Friday, Saturday, Sunday";
-
-if (lang === "ru") {
-    console.log(russianDaysOfTheWeek);
-} else if (lang === "en") {
-    console.log(englishDaysOfTheWeek);
-} else {
-    console.log("Неизвестный язык");
+'use strict'
+ 
+const allDays = function () {
+    let week = ['Воскресенье','Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'];
+    alert(String(week))
+    
+    for (let i = 0; i < week.length; i++){
+        if (week[i] === "Воскресенье" || week[i] === "Суббота" ){
+            week [i]= `<i>${week[i]}</i>`;
+        }
+        let currentDay = new Date().getDay()
+        if (i === currentDay){
+            week[i] = `<b>${week[i]}</b>`
+        }
+        console.log(week[i]); 
+       
+    }
 }
-
-// через switch
-switch (lang) {
-    case "ru":
-        console.log(russianDaysOfTheWeek);
-        break
-    case "en":
-        console.log(englishDaysOfTheWeek);
-        break
-    default:
-        console.log("Неизвестный язык");
-}
-        
-// switch второй вариант
-switch (true) {
-    case lang === "ru":
-        console.log(russianDaysOfTheWeek);
-        break
-    case lang === "en":
-        console.log(englishDaysOfTheWeek);
-        break
-    default:
-        console.log("Неизвестный язык");
-}
-
-// без if и switch
-let daysOfWeek = {
-    ru: ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'],
-  en: ['Monday', 'Tuesday', 'Wednesday', 'Thueday', 'Friday', 'Saturday', 'Sunday']
-};
-  console.log(daysOfWeek[lang]);
-
-// задание 2 через тернарные операторы
-let namePerson = prompt("Введите имя:")
-let role = (namePerson === "Артём") ? "директор" : (namePerson === "Александр") ? "преподаватель" : "студент";
-console.log(role);
+allDays()
 
 
 
 
-
->>>>>>> lesson03
