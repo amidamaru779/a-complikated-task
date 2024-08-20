391c7237f135921d060fed861ba5058dbb2b6406
Nikita Polev <nikitko779@gmail.com>
2024-08-20 17:52:58 +0300

    :переделал задание 7

--- script.js
+++ script.js
@@ -1,22 +1,39 @@
 'use strict'
  
-const allDays = function () {
-    let week = ['Воскресенье','Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'];
-    alert(String(week))
-    
-    for (let i = 0; i < week.length; i++){
-        if (week[i] === "Воскресенье" || week[i] === "Суббота" ){
-            week [i]= `<i>${week[i]}</i>`;
-        }
-        let currentDay = new Date().getDay()
-        if (i === currentDay){
-            week[i] = `<b>${week[i]}</b>`
-        }
-        console.log(week[i]); 
-       
+const appData = {
+    week: [],
+
+    start: function () {
+        appData.allDays()
+    },
+
+    allDays: function () {
+        appData.week = ["Воскресенье", "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота"];
+        const currentDay = new Date().getDay()  
+        const container = document.createElement ("div")
+        
+        appData.week.forEach(function(day, index) {
+            const dayElement = document.createElement("p")
+            if (index === currentDay){
+                dayElement.style.fontWeight = "bolder"
+            }
+
+            if (index === 0 || index === 6){
+                dayElement.style.fontStyle = "italic"
+            }
+
+            dayElement.textContent = day
+            container.appendChild(dayElement)
+            
+        })
+
+        document.body.appendChild(container)
     }
 }
-allDays()
+appData.start()
+
+
+
 
 
 
